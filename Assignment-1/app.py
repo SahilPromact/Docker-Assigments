95402d7fb3b540c9511b33d6a327ae009293c758
SahilPromact <sahil@promactinfo.com>
2025-11-06 15:57:37 +0530

    Remove ruff check

--- Assignment-1/app.py
+++ Assignment-1/app.py
@@ -3,9 +3,11 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return f"Hello, Docker! ENV: {os.getenv('APP_ENV', 'development')}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
